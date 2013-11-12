40eb80aa811352113487e67671c1229231c6e9a9
salokin <nguida91@gmail.com>
2013-11-12 16:31:03 -0500

    Added shape inside shape detection
    Edited the default source to detect if a shape is inside another shape
    and change the type of the outside shape (e.g. a triangle inside a
    rectangle is a text div, circle inside rectangle is image).

--- UI Project/UI Project/Source.cpp
+++ UI Project/UI Project/Source.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <algorithm>
 
 using namespace cv;
 using namespace std;
@@ -12,18 +13,42 @@ int thresh = 100;
 int max_thresh = 255;
 RNG rng(12345);
 
+// x value sort parameter function
+bool pointSortX(const Point& a, const Point& b) {
+	return (a.x < b.x);
+}
+
+// y value sort parameter function 
+bool pointSortY(const Point& a, const Point& b) {
+	return (a.y < b.y);
+}
+
+/*
+*@points the vertices of the shape
+*@size the number of vertices in the shape
+*@type the type of div, default to 'd' (changed later)
+*/
 struct myShape {
 	vector<Point> points;
 	size_t size;
-	myShape(vector<Point> points) {
-		this->points = points;
-		this->size = this->points.size();
+	char type;
+	myShape(vector<Point> points1) {
+		this->points = points1;
+		this->size = points1.size();
+		this->type = 'd'; // d for default
+	}
+
+	//function used to sort the vertices first by x values, then by y values
+	void pointSort() {
+		std::sort(points.begin(), points.end(), pointSortX);
+		std::sort(points.begin(), points.end(), pointSortY);
 	}
 };
+vector<myShape> myShapes; // vector to hold shapes
+vector<myShape> deletedShapes; // second vector hold deleted shapes, so you can undo the delete if you want
 
-vector<myShape> myShapes;
-
-// check for duplicates function, returns true if duplicate, otherwise returns false
+//check to make sure there are no duplicates in the shapesvector.
+// returns true if it's a duplicate, false otherwise
 bool checkShapes(vector<Point> shapeVector) {
 	int count = 0;
 	size_t shapeSize = shapeVector.size();
@@ -43,7 +68,7 @@ bool checkShapes(vector<Point> shapeVector) {
 						count += 1;
 						alreadyUsed[g] = true;
 						break;
-					}					
+					}
 				}
 			}
 			if (count == shapeSize) {
@@ -56,7 +81,55 @@ bool checkShapes(vector<Point> shapeVector) {
 		}
 	}
 	delete[] alreadyUsed;
-	return false; 
+	return false;
+}
+
+// checks for the shape type, meaning it detects if there is a shape
+// inside another shape, and if true, changes the type of the outside shape
+// to an image or text div
+void checkShapeType() {
+	double xrange1, xrange2, yrange1, yrange2;
+	int count = 0;
+	for (int i = 0; i < myShapes.size(); i++) {
+		if (myShapes[i].type == 'r') {
+			xrange1 = myShapes[i].points[0].x;
+			xrange2 = myShapes[i].points[3].x;
+			yrange1 = myShapes[i].points[0].y;
+			yrange2 = myShapes[i].points[3].y;
+			for (int j = 0; j < myShapes.size(); j++) {
+				if (myShapes[j].type == 't') {
+					for (int y = 0; y < 3; y++) {
+						if (myShapes[j].points[y].x < xrange2 && myShapes[j].points[y].x > xrange1 && myShapes[j].points[y].y < yrange2 && myShapes[j].points[y].y > yrange1)
+							count += 1;
+					}
+					if (count == 3) {
+						myShapes[i].type = 'T'; // big T indicating its a text object
+						count = 0; // reset variables
+						myShapes.erase(myShapes.begin() + j); // erase useless shape
+						i -= 1; // reset i since myShapes is now smaller in size
+						j -= 1; // reset j since myShapes is now smaller in size
+						break; // break out of for loop
+					}
+				}
+				else if (myShapes[j].type == 'c') {
+					for (int y = 0; y < myShapes[j].size; y++) {
+						if (myShapes[j].points[y].x < xrange2 && myShapes[j].points[y].x > xrange1 && myShapes[j].points[y].y < yrange2 && myShapes[j].points[y].y > yrange1)
+							count += 1;
+					}
+					if (count == myShapes[j].size) {
+						myShapes[i].type = 'I'; // big I indicating its an image
+						count = 0; // reset variables
+						myShapes.erase(myShapes.begin() + j); // erase useless shape
+						i -= 1; // reset i since myShapes is now smaller in size
+						j -= 1; // reset j since myShapes is now smaller in size
+						break; // break out of for loop
+					}
+				}
+
+			}
+
+		}
+	}
 }
 
 /// Function header
@@ -93,25 +166,34 @@ void thresh_callback(int, void*)
 	vector<Point> approxShape;
 
 	/// Detect edges using canny
-	Canny(src_gray, canny_output, thresh, thresh * 2, 3);
-	/// Find contours
-	findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-
+	Canny(src_gray, canny_output, 0, 50, 5);
+	/// Find contours	
+	findContours(canny_output, contours, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
 	/// Draw contours
 	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
 	for (int i = 0; i < contours.size(); i++)
-	{
-		approxPolyDP(contours[i], approxShape, arcLength(Mat(contours[i]), true)*0.05, true); // puts the vertexes of the contour into approxShape
-			if (checkShapes(approxShape)) { // shape is already in the display shape list, meaning it's a duplicate
-				// do nothing because it's a duplicate
+	{		
+		approxPolyDP(Mat(contours[i]), approxShape, arcLength(Mat(contours[i]), true)*0.02, true);
+		if (checkShapes(approxShape)) {
+			// do nothing because it's a duplicate
+		}
+		else {
+			myShape newShape(approxShape);
+			newShape.pointSort();
+			if (approxShape.size() == 3)
+				newShape.type = 't'; // t for triangle
+			else if (approxShape.size() == 4)
+				newShape.type = 'r'; // r for rectangle
+			else {// it's a circle
+				newShape.type = 'c'; // c for circle				
 			}
-			else {
-				myShape newShape(approxShape); // create te new shape given the vertices
-				myShapes.push_back(newShape); // add to shape list
-				Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)); 
-				drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());	// draws it... not really needed other then to show it worked			
-			}			
+			myShapes.push_back(newShape);
+			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+			drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+		}
 	}
+
+	checkShapeType();
 
 	/// Show in a window
 	namedWindow("Contours", CV_WINDOW_AUTOSIZE);
